e447631d0dfbd11cfd0f508555c5c37be1782dfc
Denis <34310801+Oslomayor@users.noreply.github.com>
2018-06-02 16:13:20 +0800

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// 字符串输出
+int main ()
+{
+	
+    char greeting[] = {'H','e','l','l','o','\0'};
+	cout << greeting ;
+	cout << " Francese" ;
+
+    return 0;
+}
